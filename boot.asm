8daadb0ab45234a38d58130f1062a6299e1a89eb
Justin Ottesen <justinottesen@gmail.com>
2026-03-10 21:53:05 -0400

    Print hello

--- boot.asm
+++ boot.asm
@@ -1,11 +1,26 @@
 ;
-; A simple boot sector program that loops forever
+; Prints hello, then loops forever
 ;
 
-; Loop forever
-loop:
-    jmp loop
+mov ah, 0x0E                    ; Set teletype mode
 
-times 510 - ($ - $$) db 0
+mov al, 'H'                     ; Load a char
+int 0x10                        ; Call bios interrupt to print
+mov al, 'e'                     ; Repeat for remaining letters
+int 0x10
+mov al, 'l'
+int 0x10
+mov al, 'l'
+int 0x10
+mov al, 'o'
+int 0x10
 
-dw 0xAA55
+jmp $                           ; Jump to current address
+
+;
+; Padding and magic BIOS number
+;
+
+times 510 - ($ - $$) db 0       ; Pad the rest of the boot sector with 0s
+
+dw 0xAA55                       ; Write the magic number so BIOS can find us
