ccff1a11a18e74ead4f736b75117c6f50e324882
Justin Ottesen <justinottesen@gmail.com>
2026-03-10 22:24:31 -0400

    Use print function for hello world

--- boot.asm
+++ boot.asm
@@ -2,20 +2,53 @@
 ; Prints hello, then loops forever
 ;
 
-mov ah, 0x0E                    ; Set teletype mode
+[org 0x7C00]                    ; The BIOS loads us at this address
 
-mov al, 'H'                     ; Load a char
-int 0x10                        ; Call bios interrupt to print
-mov al, 'e'                     ; Repeat for remaining letters
-int 0x10
-mov al, 'l'
-int 0x10
-mov al, 'l'
-int 0x10
-mov al, 'o'
-int 0x10
+; Entry point
+start:
+    ; The BIOS may leave segment registers in a weird state
+    xor ax, ax
+    mov ds, ax
+    mov es, ax
 
-jmp $                           ; Jump to current address
+    mov si, HELLO_WORLD_STRING      ; Print the hello world string
+    call print_string
+
+    jmp $                           ; Jump to current address
+
+;
+; Prints a string to the screen
+;
+; Params:
+;   - ds:si points to the string
+;
+print_string:
+    push si
+    push ax
+    push bx
+
+    mov ah, 0x0E                ; Set teletype mode
+.loop:
+    lodsb                       ; Loads the next character into al
+    or al, al                   ; Check if we are at null terminator
+    jz .done                    ; Jump to done if so
+
+    int 0x10                    ; Interrupt to print a char
+
+    jmp .loop
+
+.done:
+    pop bx
+    pop ax
+    pop si
+    ret
+
+;
+; Program Data
+;
+
+HELLO_WORLD_STRING:
+    db "Hello, world!", 0       ; Null-terminated so we know when the string ends
 
 ;
 ; Padding and magic BIOS number
