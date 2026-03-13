989ec60ce0659cc1aebda94d630aa5ed00f71d30
Justin Ottesen <justinottesen@gmail.com>
2026-03-13 15:41:37 -0400

    Read sector 2 holding hello world string

--- Makefile
+++ Makefile
@@ -1,11 +1,18 @@
 .PHONY: all run clean
-all: boot.bin
+
+all: disk.img
+
+disk.img: boot.bin stage2.bin
+	cat boot.bin stage2.bin > disk.img
 
 boot.bin: boot.asm
 	nasm boot.asm -f bin -o boot.bin
 
-run: boot.bin
-	qemu-system-x86_64 boot.bin
-	
+stage2.bin: stage2.asm
+	nasm stage2.asm -f bin -o stage2.bin
+
+run: disk.img
+	qemu-system-x86_64 -drive format=raw,file=disk.img
+
 clean:
-	rm boot.bin
+	rm -f boot.bin stage2.bin disk.img
--- boot.asm
+++ boot.asm
@@ -1,5 +1,5 @@
 ;
-; Prints hello, then loops forever
+; Loads sector 1 into 0x7E00 and prints the string stored there
 ;
 
 [org 0x7C00]                    ; The BIOS loads us at this address
@@ -10,19 +10,74 @@ start:
     xor ax, ax
     mov ds, ax
     mov es, ax
+    mov [boot_drive], dl        ; BIOS stores the boot drive number in dl
 
-    mov si, hello_world_string
-    mov cx, hello_world_length
+    mov ax, 1                   ; LBA sector 1
+    mov cl, 1                   ; read 1 sector
+    mov bx, 0x7E00              ; destination address
+    call disk_read
+
+    mov cx, [0x7E00]            ; first 2 bytes are the string length
+    mov si, 0x7E02              ; string data follows
     call print_string
 
-    jmp $                           ; Jump to current address
+    jmp $                       ; Jump to current address
+
+;
+; Reads sectors from disk
+;
+; Params:
+;   ax = LBA sector number
+;   cl = number of sectors to read
+;   bx = destination address (in segment es)
+;
+disk_read:
+    push ax
+    push bx
+    push cx
+    push dx
+
+    mov [.sector_count], cl
+
+    ; Convert LBA to CHS
+    ; sector   = (LBA % sectors_per_track) + 1
+    ; head     = (LBA / sectors_per_track) % heads_per_cylinder
+    ; cylinder = (LBA / sectors_per_track) / heads_per_cylinder
+    xor dx, dx
+    mov cx, 18                  ; sectors per track (floppy geometry)
+    div cx                      ; ax = LBA / 18, dx = LBA % 18
+    inc dx
+    mov cl, dl                  ; cl = sector number (1-based)
+
+    xor dx, dx
+    mov cx, 2                   ; heads per cylinder
+    div cx                      ; ax = cylinder, dx = head
+    mov ch, al                  ; ch = cylinder
+    mov dh, dl                  ; dh = head
+
+    mov ah, 0x02
+    mov al, [.sector_count]
+    mov dl, [boot_drive]
+    int 0x13
+    jc .error
+
+    pop dx
+    pop cx
+    pop bx
+    pop ax
+    ret
+
+.error:
+    hlt
+
+.sector_count: db 0
 
 ;
 ; Prints a string to the screen
 ;
 ; Params:
-;   - ds:si = address of the string
-;   - cx    = length of the string
+;   ds:si = address of the string
+;   cx    = length of the string
 ;
 print_string:
     push si
@@ -43,8 +98,7 @@ print_string:
 ; Program Data
 ;
 
-hello_world_string: db "Hello, world!"
-hello_world_length: equ $ - hello_world_string
+boot_drive: db 0
 
 ;
 ; Padding and magic BIOS number
